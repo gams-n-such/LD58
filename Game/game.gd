947e36069a1c4eafa8b457484aae41bcde540bad
Denis Maximenko <manlok71@gmail.com>
2025-10-04 18:24:47 +0400

    Add Game autoload

---
+++ Game/game.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var title_scene : PackedScene
+@export var gameplay_scene : PackedScene
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("dev_exit"):
+		exit()
+
+func restart(from_title : bool = true) -> void:
+	get_tree().change_scene_to_packed(title_scene if from_title else gameplay_scene)
+
+func exit() -> void:
+	get_tree().quit()
