b60cf20569741d9a0fc0c45dd90f2df500237d84
Denis Maximenko <manlok71@gmail.com>
2025-10-05 17:05:09 +0400

    Tweak curtains code in title screen

--- Title/title_screen.gd
+++ Title/title_screen.gd
@@ -4,28 +4,31 @@ extends Node
 @onready var MAIN_MENU := %MainMenu
 @onready var CREDITS := %Credits
 @onready var RETURN_BUTTON := %ReturnButton
+
 @onready var CURTAIN := %Curtain
+var default_curtain_fade := 3.0
 
 func _ready() -> void:
 	Game.set_main_ambience_playing(true, 5.0)
 	await CURTAIN.fade_out(5)
 
 func start_game() -> void:
-	pass
+	await CURTAIN.fade_in(default_curtain_fade)
+	Game.start_intro()
 
 func show_credits() -> void:
-	await CURTAIN.fade_in(3)
+	await CURTAIN.fade_in(default_curtain_fade)
 	MAIN_MENU.visible = false
 	CREDITS.visible = true
 	RETURN_BUTTON.visible = true
-	await CURTAIN.fade_out(3)
+	await CURTAIN.fade_out(default_curtain_fade)
 
 func return_to_main_menu() -> void:
-	await CURTAIN.fade_in(3)
+	await CURTAIN.fade_in(default_curtain_fade)
 	MAIN_MENU.visible = true
 	CREDITS.visible = false
 	RETURN_BUTTON.visible = false
-	await CURTAIN.fade_out(3)
+	await CURTAIN.fade_out(default_curtain_fade)
 
 func _on_play_button_pressed() -> void:
 	start_game()
