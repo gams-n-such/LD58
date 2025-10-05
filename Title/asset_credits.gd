a65b5e890135023246f1f2c7f1e3f2c6ef2c83a6
Denis Maximenko <manlok71@gmail.com>
2025-10-05 14:51:02 +0400

    Add resource scripts for asset credits

---
+++ Title/asset_credits.gd
@@ -0,0 +1,9 @@
+class_name AssetCredits
+extends Resource
+
+@export var assets : Array[Resource]
+@export var asset_name : String
+@export var author : String
+@export var source : String
+@export var source_url : String
+@export var license : String
---
+++ Title/people_credits.gd
@@ -0,0 +1,6 @@
+class_name PeopleCredits
+extends Resource
+
+@export var first_name : String
+@export var last_name : String
+@export var role : String
