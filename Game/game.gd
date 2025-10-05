fdaee88c1c228af7614a3d03cf1943514718a24a
Denis Maximenko <manlok71@gmail.com>
2025-10-05 13:35:15 +0400

    Add code to "uncapture" mouse

--- Game/game.gd
+++ Game/game.gd
@@ -10,6 +10,8 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("dev_exit"):
 		exit()
+	if event.is_action_pressed("pause"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED
 
 func restart(from_title : bool = true) -> void:
 	get_tree().change_scene_to_packed(title_scene if from_title else gameplay_scene)
