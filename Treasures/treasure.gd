27e4bba9ecd792cc8518b18127e0bc7f41ae69f2
Denis Maximenko <manlok71@gmail.com>
2025-10-05 21:29:34 +0400

    Update Treasure visuals in editor

--- Treasures/treasure.gd
+++ Treasures/treasure.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Treasure
 extends Area3D
 
@@ -9,16 +10,11 @@ extends Area3D
 		return resource
 	set(new_res):
 		resource = new_res
-		update_visuals()
+		if is_inside_tree():
+			update_visuals()
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	update_visuals()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func update_visuals() -> void:
 	if not is_node_ready():
