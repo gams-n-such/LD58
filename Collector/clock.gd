6120130ac19b8853cc0489bcd2cf622dab0a8f60
Denis Maximenko <manlok71@gmail.com>
2025-10-06 20:57:55 +0400

    Count total clock ticks

--- Collector/clock.gd
+++ Collector/clock.gd
@@ -7,6 +7,7 @@ extends Node3D
 signal tick(time_since_last_tick : float, time_to_next_tick : float)
 
 var last_tick_time : float = 0.0
+var num_ticks : int = 0
 
 func _ready() -> void:
 	Game.clock = self
@@ -29,9 +30,13 @@ func set_tick_rate(bpm : float) -> void:
 		start()
 
 func _on_tick_timer_timeout() -> void:
+	perform_tick()
+
+func perform_tick() -> void:
 	play_sound()
 	var prev_time := last_tick_time
 	last_tick_time = _get_time_sec()
+	num_ticks = num_ticks + 1
 	tick.emit(last_tick_time - prev_time, TIMER.wait_time)
 
 func play_sound() -> void:
