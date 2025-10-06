d5f40c58c7ffac33b2f085b0decc8665f395d1a7
Denis Maximenko <manlok71@gmail.com>
2025-10-06 17:00:32 +0400

    Implement treasures rising

--- Level/main_level.gd
+++ Level/main_level.gd
@@ -13,5 +13,7 @@ func _ready() -> void:
 	await first_shrine.item_changed
 	for shrine in get_tree().get_nodes_in_group("Shrines"):
 		(shrine as Shrine).erect()
-	await get_tree().create_timer(3).timeout
+	var treasures_tween := get_tree().create_tween()
+	treasures_tween.tween_property(%TreasuresRoot, "global_position", Vector3.UP * 0.8, 3)
+	await treasures_tween.finished
 	CLOCK.start()
