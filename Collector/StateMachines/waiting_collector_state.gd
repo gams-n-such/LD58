32157ed08e37eb36ecaf2c06e2e6c992a5530a68
Denis Maximenko <manlok71@gmail.com>
2025-10-06 20:58:35 +0400

    Implement WaitingCollectorState

--- Collector/StateMachines/waiting_collector_state.gd
+++ Collector/StateMachines/waiting_collector_state.gd
@@ -1,13 +1,19 @@
 extends CollectorState
 
+@export var wait_ticks : int = 5
+
+var remaining_ticks : int = 5
 
 func _ready() -> void:
 	super._ready()
 
 func enter(prev_state : State) -> void:
 	super.enter(prev_state)
+	collector.aim_spotlight.visible = false
+	remaining_ticks = wait_ticks
 
 func exit(next_state : State) -> void:
+	collector.aim_spotlight.visible = true
 	super.exit(next_state)
 
 func update(delta: float) -> void:
@@ -18,3 +24,6 @@ func physics_update(delta: float) -> void:
 
 func tick(time_since_last_tick : float, time_to_next_tick : float) -> void:
 	super.tick(time_since_last_tick, time_to_next_tick)
+	remaining_ticks -= 1
+	if remaining_ticks <= 0:
+		request_transition("TargetingCollectorState")
